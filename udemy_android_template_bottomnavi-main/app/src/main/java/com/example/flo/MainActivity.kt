e0e89d90f413ad34291386c6159f653ba6f6471c
sum130 <sm3904@naver.com>
2023-10-03 05:49:05 +0900

    #4 mission_indicator

--- udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/MainActivity.kt
+++ udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/MainActivity.kt
@@ -23,10 +23,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FLO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString())
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString(), 0,60,false)
 
 
         binding.mainPlayerCl.setOnClickListener{
@@ -34,7 +35,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SongActivity::class.java)
             intent.putExtra("title", song.title)
             intent.putExtra("singer", song.singer)
-            //startActivity(intent)
+            intent.putExtra("second", song.second)
+            intent.putExtra("playTime", song.playTime)
+            intent.putExtra("isPlaying", song.isPlaying)
+            startActivity(intent)
             getResultText.launch(intent)
         }
 
--- udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/SongActivity.kt
+++ udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/SongActivity.kt
@@ -11,10 +11,16 @@ import com.example.flo.databinding.ActivitySongBinding
 class SongActivity : AppCompatActivity() {
 
     lateinit var binding : ActivitySongBinding
+    lateinit var song : Song
+    lateinit var timer : Timer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initSong()
+        setPlayer(song)
 
         binding.songDownIb.setOnClickListener{
             val intent = Intent(this, SongActivity::class.java)
@@ -39,13 +45,45 @@ class SongActivity : AppCompatActivity() {
         binding.songPauseIv.setOnClickListener{
             setPlayerStatus(true)
         }
-
-        if(intent.hasExtra("title")&& intent.hasExtra("singer")){
-            binding.songMusicTitleTv.text= intent.getStringExtra("title")
-            binding.songSingerNameTv.text= intent.getStringExtra("singer")
-        }
     }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        timer.interrupt()
+    }
+
+    private fun initSong(){
+        if(intent.hasExtra("title")&& intent.hasExtra("singer")){
+            song = Song(
+                intent.getStringExtra("title")!!,
+                intent.getStringExtra("singer")!!,
+                intent.getIntExtra("second", 0),
+                intent.getIntExtra("playTime", 0),
+                intent.getBooleanExtra("isPlaying", false)
+
+            )
+        }
+        startTimer()
+    }
+
+    private fun setPlayer(song:Song){
+        binding.songMusicTitleTv.text= intent.getStringExtra("title")!!
+        binding.songSingerNameTv.text= intent.getStringExtra("singer")!!
+        binding.songStartTimeTv.text = String.format("%02d:%02d", song.second /60, song.second%60)
+        binding.songEndTimeTv.text = String.format("%02d:%02d", song.playTime /60, song.playTime%60)
+        binding.songProgressSb.progress = (song.second *1000/song.playTime)
+
+        setPlayerStatus(song.isPlaying)
+
+    }
+
+
+
+
     fun setPlayerStatus(isPlaying : Boolean){
+        song.isPlaying = isPlaying
+        timer.isPlaying = isPlaying
+
         if(isPlaying){
             binding.songMiniplayerIv.visibility = View.VISIBLE
             binding.songPauseIv.visibility = View.GONE
@@ -53,6 +91,7 @@ class SongActivity : AppCompatActivity() {
         else{
             binding.songMiniplayerIv.visibility = View.GONE
             binding.songPauseIv.visibility = View.VISIBLE
+
         }
     }
 
@@ -64,6 +103,48 @@ class SongActivity : AppCompatActivity() {
         else{
             binding.songRepeatIv.visibility = View.GONE
             binding.songRandomIv.visibility = View.VISIBLE
+        }
+    }
+
+
+    private fun startTimer(){
+        timer = Timer(song.playTime, song.isPlaying)
+        timer.start()
+    }
+    inner class Timer(private val playTime : Int, var isPlaying: Boolean = true):Thread(){
+
+        private var second : Int = 0
+        private var mills: Float = 0f
+
+        override fun run(){
+            super.run()
+            try{
+
+                while(true){
+                    if(second >=playTime){
+                        break
+                    }
+                    if(isPlaying){
+                        sleep(50)
+                        mills += 50
+
+                        runOnUiThread(){
+                            binding.songProgressSb.progress = ((mills /playTime)*100).toInt()
+                        }
+                        if(mills % 1000==0f){
+                            runOnUiThread{
+                                binding.songStartTimeTv.text = String.format("%02d:%02d", second /60, second%60)
+                            }
+                            second++
+                        }
+                    }
+                }
+
+            }catch(e: InterruptedException){
+                Log.d("Song", "쓰레드가 죽었습니다. ${e.message}")
+
+            }
+
         }
     }
 }
