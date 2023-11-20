98eead6e8173020f20f13181282f837fc134209b
sum130 <sm3904@naver.com>
2023-11-21 01:44:00 +0900

    #8 Week8

--- udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/LockerFragment.kt
+++ udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/LockerFragment.kt
@@ -10,6 +10,10 @@ import com.example.flo.databinding.FragmentLockerBinding
 import com.google.android.material.tabs.TabLayoutMediator
 import java.util.concurrent.locks.Lock
 import androidx.viewpager2.widget.ViewPager2
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 
 class LockerFragment : Fragment() {
@@ -17,7 +21,7 @@ class LockerFragment : Fragment() {
     lateinit var binding: FragmentLockerBinding
     //private var albumDatas = ArrayList<LockerMusic>()
 
-    private val information = arrayListOf("저장한 곡", "음악파일")
+    private val information = arrayListOf("저장한 곡", "음악파일", "저장앨범")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,11 +57,58 @@ class LockerFragment : Fragment() {
 
         val lockerAdapter = LockerVPAdapter(this)
         binding.restoreContentVp.adapter = lockerAdapter
-        TabLayoutMediator(binding.restoreContentTb, binding.restoreContentVp){
+        TabLayoutMediator(binding.lockerContentTb, binding.lockerContentVp){
                 tab, position ->
             tab.text = information[position]
         }.attach()
 
+
+        binding.lockerLoginTv.setOnClickListener {
+            startActivity(Intent(activity, LoginActivity::class.java))
+        }
+
+
+        
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val jwt: Int = getJwt()
+
+        if (jwt == 0){
+            binding.lockerLoginTv.text = "로그인"
+
+            binding.lockerLoginTv.setOnClickListener {
+                startActivity(Intent(activity, LoginActivity::class.java))
+            }
+        }
+        else{
+            binding.lockerLoginTv.text = "로그아웃"
+
+            binding.lockerLoginTv.setOnClickListener {
+                logout()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }
+    }
+
+    private fun getJwt(): Int {
+        val spf = activity?.getSharedPreferences("auth" , AppCompatActivity.MODE_PRIVATE)
+
+        return spf!!.getInt("jwt", 0)
+    }
+
+    private fun logout() {
+        val spf = activity?.getSharedPreferences("auth" , AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+
+        editor.remove("jwt")
+        editor.apply()
     }
 }
